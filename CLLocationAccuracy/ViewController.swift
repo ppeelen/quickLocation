0a35b49ec2d1de8ff0bbaf2ff20876a8e4a4c882
Paul Peelen <paul@paulpeelen.com>
2021-01-24 17:13:19 +0100

    Added a quick project for precise location

--- CLLocationAccuracy/ViewController.swift
+++ CLLocationAccuracy/ViewController.swift
@@ -1,19 +1,76 @@
-//
-//  ViewController.swift
-//  CLLocationAccuracy
-//
-//  Created by Paul Peelen on 2021-01-24.
-//
-
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  @IBOutlet weak var authStatus: UILabel!
+  @IBOutlet weak var exactPositionEnabled: UILabel!
 
+  private let locationManager = CLLocationManager()
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setup()
+  }
+
+  @IBAction func openSettings(_ sender: Any) {
+    let string = UIApplication.openSettingsURLString.appending("?root=Privacy&path=LOCATION")
+    UIApplication.shared.open(URL(string: string)!)
+  }
 }
 
+private extension ViewController {
+
+  func setup() {
+    locationManager.delegate = self
+
+    setTexts()
+
+    locationManager.desiredAccuracy = .greatestFiniteMagnitude
+    locationManager.requestAlwaysAuthorization()
+
+    if CLLocationManager.locationServicesEnabled() {
+      locationManager.startUpdatingLocation()
+    }
+  }
+
+  func setTexts() {
+    let authStatusText: String
+
+    switch locationManager.authorizationStatus {
+    case .authorizedAlways:
+      authStatusText = "Always"
+    case .authorizedWhenInUse:
+      authStatusText = "When in use"
+    case .denied:
+      authStatusText = "Denied"
+    case .notDetermined:
+      authStatusText = "Not determined"
+    case .restricted:
+      authStatusText = "Restricted"
+    @unknown default:
+      authStatusText = "-"
+    }
+
+    authStatus.text = authStatusText
+
+    let accuracyText: String
+    switch locationManager.accuracyAuthorization {
+    case .fullAccuracy:
+      accuracyText = "Yes, Full"
+    case .reducedAccuracy:
+      accuracyText = "No, reduced"
+    @unknown default:
+      accuracyText = "-"
+    }
+
+    exactPositionEnabled.text = accuracyText
+  }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    setTexts()
+  }
+}
